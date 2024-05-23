f2002537e026722f07249be349e7f7da5f744770
Tom Morskieft <tommorskieft@outlook.com>
2024-05-23 20:57:14 +0200

    Add all new packages

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Hello from Vue.js Component</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.');
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: blue;
+}
+</style>
